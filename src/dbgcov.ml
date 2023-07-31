486a256c297be4a8fe98a5929ca8a47bbb9e3736
Stephen Kell <srk31@srcf.ucam.org>
2023-07-31 16:29:42 +0100

    dbgcov.ml: avoid use of temporary output file

--- src/dbgcov.ml
+++ src/dbgcov.ml
@@ -15,47 +15,31 @@ open Unix
 let () =
     let argList = Array.to_list Sys.argv in
     let (argChunks, basicInfo) = scanAndChunkCppArgs argList in
-    let cppCommandPrefix, guessedLang = guessCppCommandAndLang basicInfo in
-    let suffix = match guessedLang with
-        "c++" -> "ii"
-      | "c" -> "i" (* FIXME: other languages are possible *)
-      | _ -> failwith (guessedLang ^ " is not a language")
-    in
-    let (newTempFd, newTempName) = mkstemps ("/tmp/tmp.XXXXXX.cpp." ^ suffix)
-        (String.length (".cpp." ^ suffix)) in
-    let rewrittenArgs = List.flatten (List.mapi (fun i -> fun argChunk ->
-        if i = 0 then [] (* we fill "cpp" or whatever from cppCommandPrefix *) else
-        match argChunk with
-          | ["-o"; filename] -> ["-o"; newTempName]
-          | _ -> argChunk) argChunks)
-      @ ( (* we might not have seen "-o" -- ensure there is a -o argument *)
-      match basicInfo.minus_o_pos with
-        None -> (* there was no -o, so add one *) [ "-o"; newTempName ]
-      | _ -> [])
-    in
-    (* We actually don't want to divert cpp to a temporary file.
-     * Can we make that an option? *)
-    let infd = openfile newTempName [O_RDONLY] 0o640 in
-    (* delete temporary file unless -save-temps *)
-    (*let () = if saveTemps then () else unlink newTempName in *)
-    (* dup2 our stdout to the original out file, and our stdin to the temp *)
-    (* First we copy the prelude to the output. If we don't have a prelude
-     * argument (FIXME: look for one in ppPassesToRun) we use the default. *)
-    let () = match basicInfo.output_file with
-        Some(fname) -> let outFname = (fname ^ ".dbgcov") in
-                       let outfd = Unix.openfile outFname [O_RDWR; O_CREAT] 0o640 in
-            ((output_string Pervasives.stderr ("output should go to " ^ outFname ^ "\n"); Pervasives.flush Pervasives.stderr);
-            dup2 outfd stdout)
-          | None -> (
-            output_string Pervasives.stderr ("output should go to stdout \n"); Pervasives.flush Pervasives.stderr;
-            ()
-            )
-    in
-    let () = dup2 infd stdin in
-    let toolPath = if Filename.is_relative Sys.argv.(0) (* TODO: perhaps try Sys.executable_name ? *)
-        then Filename.concat Filename.current_dir_name "dbgcov-tool"
-        else Filename.concat (Filename.dirname Sys.executable_name) "dbgcov-tool"
-    in
-    (* FIXME: pass "/dev/stdin" if we can figure out how to tell clang it's C++ *)
-    execv toolPath [|toolPath; newTempName ; "--"|]
-
+    (* We actually don't want to divert cpp to a temporary file per se,
+     * unlike cccppp which really adds a new stage to the pipeline.
+     * Instead we want to 'tap' the output of cpp so that as well as its
+     * being fed onwards to cc1, we also run it through dbgcov. How can
+     * we achieve that? The simplest way is to ensure we output to a
+     * file, and run our tool on that file before exiting and allowing
+     * compilation to continue. *)
+    match basicInfo.output_file with
+        None -> failwith "error: cpp command has no output file name (FIXME: support this case)"
+      | Some the_input_file_name ->
+            (* fork the real cpp and then wait for it *)
+            let realArgs = List.flatten (List.mapi (fun i -> fun argChunk ->
+                if i = 0 then [] (* we fill "cpp" or whatever from cppCommandPrefix *) else argChunk
+            ) argChunks) in
+            let cppCommandPrefix, guessedLang = guessCppCommandAndLang basicInfo in
+            runCommand "cpp" (cppCommandPrefix @ realArgs);
+            (* Run our tool on cpp's output file. We redirect the tool's output
+             * to a file named after it. *)
+            let toolPath = if Filename.is_relative Sys.argv.(0) (* TODO: perhaps try Sys.executable_name ? *)
+                then Filename.concat Filename.current_dir_name "dbgcov-tool"
+                else Filename.concat (Filename.dirname Sys.executable_name) "dbgcov-tool"
+            in
+            let the_output_file_name = the_input_file_name ^ ".dbgcov" in
+            let outfd = Unix.openfile the_output_file_name [O_RDWR; O_CREAT] 0o640 in
+               ((output_string Pervasives.stderr ("output should go to " ^ the_output_file_name ^ "\n");
+                 Pervasives.flush Pervasives.stderr);
+                 dup2 outfd stdout);
+            execv toolPath [|toolPath; the_input_file_name ; "--"|]
